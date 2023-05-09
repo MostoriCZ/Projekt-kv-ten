5eeef1b78f20e3673ace878977a5080627e61f87
MostoriCZ <jakubzeidel@gmail.com>
2023-05-09 10:09:10 +0200

    Přidejte soubory projektu.

---
+++ Projekt květen/Program.cs
@@ -0,0 +1,34 @@
+﻿class Diamond
+{
+    private class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; } = "";
+        public double Price { get; set; }
+    }
+    private static class Inventory
+    {
+        public static List<Product> Products { get; set; } = new List<Product>();
+        public static int Count { get; set; }
+        public static void AddProduct(string name, double price)
+        {
+            Products.Add(new Product(name, price));
+        }
+        public static void RemoveProduct(string name, double price)
+        {
+            Products.Remove(new Product(name, price));
+
+        }
+    }
+
+    public static void Main()
+    {
+        
+    }
+}
+
